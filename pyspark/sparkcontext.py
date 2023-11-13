a7f9232471015e8ec7d8b9d648407531a0a10a37
Dang Nhat <88953246+dangnhatsimon@users.noreply.github.com>
2023-11-13 09:24:09 +0700

    Add files via upload

---
+++ pyspark/sparkcontext.py
@@ -0,0 +1,22 @@
+import pyspark
+from pyspark import SparkContext as sc
+
+# Print the version of SparkContext
+print("The version of Spark Context in the PySpark shell is", sc.version)
+
+# Print the Python version of SparkContext
+print("The Python version of Spark Context in the PySpark shell is", sc.pythonVer)
+
+# Print the master of SparkContext
+print("The master of Spark Context in the PySpark shell is", sc.master)
+
+# Create a Python list of numbers from 1 to 100 
+numb = range(1, 101)
+
+# Load the list into PySpark  
+spark_data = sc.parallelize(numb)
+
+file_path='README.md'
+# Load a local file into PySpark shell
+lines = sc.textFile(file_path)
+
